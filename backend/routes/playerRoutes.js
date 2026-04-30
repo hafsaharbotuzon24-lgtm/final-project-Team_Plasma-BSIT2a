03c2488e115d360bf34e6b526dbc7b9493462202
piapipp <sophiabalmaceda38@gmail.com>
2026-04-30 22:12:59 +0800

    Updated patch and delete for player routes

--- backend/routes/playerRoutes.js
+++ backend/routes/playerRoutes.js
@@ -7,11 +7,10 @@ router.post('/', controller.createPlayer);
 router.get('/', protect, controller.getPlayers);
 router.get('/me', protect, controller.getMyPlayer);
 router.patch('/me/resources', protect, controller.updateMyResources);
+router.patch('/me/password', protect, controller.changePassword);
+router.delete('/me', protect, controller.deleteMyAccount);
 router.get('/:id', protect, controller.getPlayer);
 router.put('/:id', protect, controller.updatePlayer);
 router.delete('/:id', protect, controller.deletePlayer);
-
-router.patch('/me/password', protect, controller.changePassword);
-router.delete('/me', protect, controller.deleteMyAccount);
 
 module.exports = router;
